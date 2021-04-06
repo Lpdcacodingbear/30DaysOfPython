1da384ec7a9d65961051fbd0aed971034c3d85a5
Saker <saker.lin@aif.tw>
2021-04-06 14:51:58 +0800

    feat: day 1
    1. Initializing a new repositor
    2. Practice print funtion
    3. Practice basic mathematic operations
    
    reference:
    - https://github.com/Asabeneh/30-Days-Of-Python
    - https://github.com/Asabeneh/Python-for-Everyone

---
+++ day_1/helloworld.py
@@ -0,0 +1,27 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # addition(+)
+print(0.1 + 0.2)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(9.8))
+print(type(1 + 3j))      # Complex number
+print(type(4 - 4j))
+print(type('Goku'))  # String
+print(type('Son'))
+print(type('Wakusei bejīta'))
+print(type([1, 2, 3]))   # List
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type({'name':'Goku'})) # Dictionary
+print(type({'name':'Son'}))
+print(type({'name':'Wakusei bejīta'}))
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
